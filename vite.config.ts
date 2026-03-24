70d4cf12a5b26a45fa28a57dd5e1eb6062038ad2
ANTHONY JAMES NABASCA <anthonyjames.nabasca@nmsc.edu.ph>
2026-03-24 13:46:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+    // Tailwind is not being actively used - do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
       // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, './src'),
     },
   },
+
+  base: '/Citypromotionalwebsiteui/',
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
